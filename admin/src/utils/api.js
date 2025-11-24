b330098a89e793a2f1561dfd7db3799eff3ecb7d
yuvi797 <152168658+yuvi797@users.noreply.github.com>
2025-11-24 19:44:20 +0530

    Update api.js

--- admin/src/utils/api.js
+++ admin/src/utils/api.js
@@ -4,6 +4,7 @@ const API_URL = import.meta.env.VITE_API_BASE_URL
 // const API_URL = 'http://localhost:5000/api';
 
 const api = axios.create({
+    withCredentials: true,
     baseURL: API_URL,
     headers: {
         'Content-Type': 'application/json',
